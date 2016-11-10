e48515ac69b08db87cce9e1b4c750f9c53d5c390
rafaelavm <rafaela.vm@gmail.com>
2016-11-10 15:09:40 -0200

    Aula 03: Instalação de pacotes via NPM

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+//Aula 02
+var nome = "Rafaela";
+
+function exibirMensagem(nome){
+    
+    console.log("Olá", nome);
+}
+
+exibirMensagem(nome);*/
+
+//Aula 03
+//no site do npm vc conseuge ver todos os pacotes que tem pra instalar e as explicações deles.
+//pra verificar a versão do npm: 
+//npm -v
+
+
+//faz referência ao pacote instalado:
+var clc = require("cli-color");
+
+console.log(clc.green("Mensagem verde;"));
+
+//buscando na documentação do pacote no site do npm:
+console.log(clc.red.bgWhite.underline('Underlined red text on white background.'));
